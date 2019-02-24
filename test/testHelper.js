26fceee7ee5712acbca5aafa2a6cf7cfe6ed354f
Nicolas Dupont <npg.dupont@gmail.com>
2019-02-24 14:06:26 +0100

    Add test stack

---
+++ test/testHelper.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+function clearCollections(done) {
+  for (var collection in mongoose.connection.collections) {
+    mongoose.connection.collections[collection].remove(function() {});
+  }
+  return done();
+}
+
+before(function (done) {
+  if (mongoose.connection.readyState === 0) {
+    mongoose.connect("mongodb://localhost/HackathonScPoTest", function (err) {
+      if (err) throw err;
+      return clearCollections(done);
+    });
+  } else {
+    return clearCollections(done);
+  }
+});
+
+after(function (done) {
+  return clearCollections(() => {
+    mongoose.disconnect();
+    return done();
+  });
+});
