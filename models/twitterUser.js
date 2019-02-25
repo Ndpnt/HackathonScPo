a5c9ded24cf048d59ed6ae678997b2ae4acbbeda
Nicolas Dupont <npg.dupont@gmail.com>
2019-02-25 09:38:39 +0100

    Add a fetchTwitter Data to twitterUser model

--- models/twitterUser.js
+++ models/twitterUser.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+const Twit = require('twit');
+const Twitter = new Twit({
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
 const twitterUserSchema = new mongoose.Schema({
   screen_name: {
     type: String,
@@ -10,6 +18,21 @@ const twitterUserSchema = new mongoose.Schema({
   twitterData: Object
 });
 
-const TwitterUser = mongoose.model('TwitterUser', twitterUserSchema);
+twitterUserSchema.methods.fetchTwitterData = function fetchTwitterData () {
+  return new Promise((resolve, reject) => {
+    Twitter.get('users/show', { screen_name: this.screen_name }, (error, twitterData, response) => {
+      if (error) {
+        return reject(error);
+      }
 
+      this.twitterData = twitterData;
+
+      this.save()
+        .then(resolve)
+        .catch(reject);
+    });
+  });
+};
+
+const TwitterUser = mongoose.model('TwitterUser', twitterUserSchema);
 module.exports = TwitterUser;
--- test/models.js
+++ test/models.js
@@ -1,3 +1,4 @@
+require('./testHelper');
 const { expect } = require('chai');
 const sinon = require('sinon');
 require('sinon-mongoose');
@@ -6,18 +7,39 @@ const TwitterUser = require('../models/twitterUser');
 
 describe('TwitterUser Model', () => {
   it('should create a new user', (done) => {
-    const TwitterUserMock = sinon.mock(new TwitterUser());
-    const twitterUser = TwitterUserMock.object;
+    const twitterUser = new TwitterUser({
+      screen_name: 'twitter',
+    });
 
-    TwitterUserMock
-      .expects('save')
-      .yields(null);
+    return twitterUser.save(function (err) {
+      if (err) {
+        done(err);
+      }
 
-    twitterUser.save((err) => {
-      TwitterUserMock.verify();
-      TwitterUserMock.restore();
       expect(err).to.be.null;
       done();
+    });
+  });
+
+  it('should fetch twitter user data', (done) => {
+    const screen_name = "twitterDev";
+    const twitterUser = new TwitterUser({
+      screen_name: screen_name,
+    });
+
+    twitterUser.save().then(() => {
+      TwitterUser.findOne({ screen_name: screen_name }, (err, user) => {
+        user.fetchTwitterData().then(() => {
+          if (err) {
+            done(err);
+          }
+
+          expect(user.twitterData).to.not.be.undefined;
+          expect(user.twitterData.screen_name.toLowerCase()).to.equal(screen_name.toLowerCase());
+          done();
+        })
+        .catch(done);
+      });
     });
   });
 });
